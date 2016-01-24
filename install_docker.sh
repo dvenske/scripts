e805da23f39bae79338f64596ff73b489ccd2d44
Deane Venske <deane@venske.net>
2016-01-24 14:30:33 -0700

    Docker setup update

--- install_docker.sh
+++ install_docker.sh
@@ -10,3 +10,4 @@ service docker start
 
 #Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
