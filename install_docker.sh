00c3c5b1b3f266f4e60002fefdba0a0459d26fd5
Deane Venske <deane@venske.net>
2016-03-10 00:15:21 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -8,6 +8,8 @@ apt-get update
 apt-get install -y docker-engine
 service docker start
 
+sudo gpasswd -a ${USER} docker
+
 #Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
